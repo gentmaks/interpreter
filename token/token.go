c405ef9889a18244995fa9ec9b9d5941ed16c622
Gent Maksutaj <gent.maksutajmax@gmail.com>
2025-12-08 16:45:22 +0100

    token struct code

---
+++ token/token.go
@@ -0,0 +1,37 @@
+// Package token defines the token struct
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	literal string
+}
+
+const (
+	// special token types
+	ILLEGALL = "ILLEGAl"
+	EOF      = "EOF"
+
+	// identifiers and literals
+	IDENT = "IDENT"
+	INT   = "INT"
+	BOOL  = "BOOL"
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	LET      = "LET"
+	FUNCTION = "FUNCTION"
+)
